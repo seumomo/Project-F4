755f96554cb67029eadb3849688b72372eed2595
yena <spffhd0835@gmail.com>
2023-06-27 10:15:00 +0900

    검색 ui

--- src/js/search.js
+++ src/js/search.js
@@ -1,8 +1,7 @@
 const searchBtn = document.querySelector('.subMenu__search');
 const searchFormWrap = document.querySelector('.searchFormWrap');
 const header = document.querySelector('.header');
-
-
+console.log(searchFormWrap);
 searchBtn.addEventListener('click',function(){
   searchFormWrap.classList.toggle('is--active');
 });
