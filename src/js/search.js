fbeade0da2ac357ca211e098a38c7200153bf52d
yena <spffhd0835@gmail.com>
2023-06-27 11:14:08 +0900

    검색폼 자바마크업 및 자바스크립트 코드 추가

--- src/js/search.js
+++ src/js/search.js
@@ -1,7 +1,9 @@
 const searchBtn = document.querySelector('.subMenu__search');
 const searchFormWrap = document.querySelector('.searchFormWrap');
 const header = document.querySelector('.header');
-console.log(searchFormWrap);
-searchBtn.addEventListener('click',function(){
+
+searchBtn.addEventListener('click',function(e){
+  e.preventDefault();
   searchFormWrap.classList.toggle('is--active');
+  header.classList.toggle('bg--gray');
 });
