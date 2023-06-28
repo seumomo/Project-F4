006721b5484dc24af484324532b6b44e13e30c49
seumomo <xo0207@naver.com>
2023-06-28 10:21:07 +0900

    리드미 수정 및 배포 준비

--- src/js/search.js
+++ src/js/search.js
@@ -1,9 +1,8 @@
-const searchBtn = document.querySelector('.subMenu__search');
-const searchFormWrap = document.querySelector('.searchFormWrap');
-const header = document.querySelector('.header');
-
-searchBtn.addEventListener('click',function(e){
+const searchBtn = document.querySelector(".subMenu__search");
+const searchFormWrap = document.querySelector(".searchFormWrap");
+const header = document.querySelector(".header");
+searchBtn.addEventListener("click", function (e) {
   e.preventDefault();
-  searchFormWrap.classList.toggle('is--active');
-  header.classList.toggle('bg--gray');
+  searchFormWrap.classList.toggle("is--active");
+  header.classList.toggle("bg--gray");
 });
